029467c958c24ba5117eaa87c6623c33dfd0b606
f-zaryab <zaryabfarooqui@gmail.com>
2025-05-31 23:50:55 +0100

    repo-setup

---
+++ Components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        Footer Content
+      </footer>
+  )
+}
+
+export default Footer
---
+++ Components/Navbar/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+
+const Navbar = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/About">About us</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default Navbar;
