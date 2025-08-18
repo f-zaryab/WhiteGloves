5fa7c877aec774f52dfb3ef41081cf366703cb9a
f-zaryab <zaryabfarooqui@gmail.com>
2025-08-18 01:09:15 +0100

    sitemap add corrected

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://whitegloves.cc/sitemap.xml",
+    sitemap: "https://www.whitegloves.cc/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://whitegloves.cc',
+      url: 'https://www.whitegloves.cc/',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
     },
     {
-      url: 'https://whitegloves.cc/about',
+      url: 'https://www.whitegloves.cc/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
